1d2b2d76d8b3d0682895d5303fc5ef73664b0e00
Nicolas Chevalier <niccheva@student.42.fr>
2015-12-19 20:33:59 +0100

    server with auth

--- git_server.go
+++ git_server.go
@@ -5,8 +5,8 @@
 //                                                    +:+ +:+         +:+     //
 //   By: niccheva <niccheva@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
-//   Created: 2015/01/04 18:39:27 by niccheva          #+#    #+#             //
-//   Updated: 2015/01/04 22:07:51 by niccheva         ###   ########.fr       //
+//   Created: 2015/12/07 18:52:47 by niccheva          #+#    #+#             //
+//   Updated: 2015/12/07 18:55:07 by niccheva         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -14,160 +14,343 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"fmt"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
+	"net/http"
+	"net/url"
 	"os"
-	"strings"
-	"io"
 	"os/exec"
+	"strings"
 	"syscall"
 
-	"code.google.com/p/go.crypto/ssh"
+	"github.com/spacemonkeygo/openssl"
 )
 
-func parseKeys(conf *ssh.ServerConfig) {
-	privateBytes, err := ioutil.ReadFile(os.Getenv("HOME") + "/.ssh/id_rsa")
-	if err != nil {
-		log.Fatal("Failed to load private key: ", err)
+var publicKey string
+var authServer string
+
+func createServer(config *ssh.ServerConfig) {
+
+	port := os.Getenv("GIT_PORT")
+	if port == "" {
+
+		port = "22"
+
 	}
 
-	private, err := ssh.ParsePrivateKey(privateBytes)
+	listener, err := net.Listen("tcp", "0.0.0.0:" + port)
 	if err != nil {
-		log.Fatal("Failed to parse private key: ", err, "\n\n Maybe your key is encrypted, please try with an not encrypted key..")
+
+		log.Fatal("Fail to listen for connections: ", err)
+
 	}
 
-	conf.AddHostKey(private)
-}
-
-func createServer(conf *ssh.ServerConfig) {
-	listener, err := net.Listen("tcp", "0.0.0.0:22")
-	if err != nil {
-		log.Fatal("Failed to listen for connection: ", err)
-	}
-
+	log.Println("git server listen to " + port + " port")
 	for {
-		nConn, err := listener.Accept()
+
+		netConn, err := listener.Accept()
 		if err != nil {
-			log.Println("Failed to accept incoming connection: ", err)
+
+			log.Println("Fail to accept incoming connection: ", err)
 			continue
+
 		}
-		go handleConnection(nConn, conf)
+
+		go handleConnection(netConn, config)
+
 	}
+
 }
 
-func handleConnection(conn net.Conn, conf *ssh.ServerConfig) {
-	defer conn.Close()
+type exitStatusMsg struct {
 
-	sshConn, chans, reqs, err := ssh.NewServerConn(conn, conf)
+	Status uint32
+
+}
+
+func exitStatus(err error) (exitStatusMsg, error) {
+
 	if err != nil {
-		log.Fatal("Failed to handshake: ", err)
+
+		if exitErr, ok := err.(*exec.ExitError); ok {
+
+			if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
+
+				return exitStatusMsg{uint32(status.ExitStatus())}, nil
+
+			}
+
+		}
+
+		return exitStatusMsg{0}, err
+
+	}
+
+	return exitStatusMsg{0}, nil
+
+}
+
+func handleChannel(connection *ssh.ServerConn, newChannel ssh.NewChannel) {
+
+	channel, reqs, err := newChannel.Accept()
+	if err != nil {
+
+		log.Println("Could not accept channel: ", err)
 		return
+
+	}
+
+	defer channel.Close()
+	for req := range reqs {
+
+		switch req.Type {
+
+		case "exec":
+			if req.WantReply {
+
+				req.Reply(true, nil)
+
+			}
+
+			log.Println(connection.User() + "@" + connection.RemoteAddr().String() + " want exec " + string(req.Payload[4:]))
+			args := strings.Split(string(req.Payload[4:]), " ")
+			if args[0] != "git-receive-pack" && args[0] != "git-upload-pack" {
+
+				channel.Stderr().Write([]byte("Sorry, this server just accept git requests.\n"))
+				return
+
+			}
+
+			args[1] = strings.Replace(args[1], "'", "", -1)
+			pathRepo := strings.Split(args[1], "/")
+			name := pathRepo[len(pathRepo)-1:][0]
+			jsonStr := []byte(fmt.Sprintf(`{"key":{"key":"%s"}, "project":{"name":"%s"}}`, publicKey, strings.TrimSuffix(name, ".git")))
+			url, err := url.Parse("http://" + authServer + ":3000/api/authorization")
+			if err != nil {
+
+				log.Println("Fail to parse url: ", err)
+				return
+
+			}
+
+			response, err := restClient("POST", url, jsonStr)
+			if err != nil {
+
+				log.Println(err)
+				return
+
+			}
+
+			if response.StatusCode != 200 {
+
+				channel.Stderr().Write([]byte("(ERROR) Its appear you have no right on this repository.\n"))
+				return
+
+			}
+
+			log.Println(connection.User() + "@" + connection.RemoteAddr().String() + " exec " + string(req.Payload[4:]))
+
+			cmd := exec.Command("git-shell", "-c", args[0] + " '/tmp/" + name + "'")
+			pipeCommand(cmd, channel, channel.Stderr(), channel)
+			cmd.Start()
+			status, err := exitStatus(cmd.Wait())
+			if err != nil {
+
+				log.Println("exitStatus")
+				return
+
+			}
+
+			if status.Status != 0 {
+
+				return
+
+			}
+
+			if _, err := channel.SendRequest("exit-status", false, ssh.Marshal(&status)); err != nil {
+
+				log.Println("sendExit")
+				return
+
+			}
+
+			return
+
+		case "env":
+			if req.WantReply {
+
+				req.Reply(true, nil)
+
+			}
+
+		case "shell":
+			channel.Stderr().Write([]byte("Sorry, this server just accept exec requests.\n"))
+			return
+
+		}
+
+	}
+
+}
+
+func handleConnection(connection net.Conn, config *ssh.ServerConfig) {
+
+	defer connection.Close()
+
+	log.Println("New connection from ", connection.RemoteAddr().String())
+
+	sshConn, chans, reqs, err := ssh.NewServerConn(connection, config)
+	if err != nil {
+
+		log.Println("Fail to handshake: ", err)
+		return
+
 	}
 
 	go ssh.DiscardRequests(reqs)
 	for ch := range chans {
+
 		if ch.ChannelType() != "session" {
+
 			ch.Reject(ssh.UnknownChannelType, "unknown channel type")
 			continue
+
 		}
+
 		go handleChannel(sshConn, ch)
+
 	}
+
 }
 
-func writeLog(buf bytes.Buffer) {
-	log.Print(&buf)
-	logfile := os.Getenv("GIT_LOGFILE")
-	if logfile == "" {
-		logfile = os.Getenv("HOME") + "/git/.log"
-	}
+func loadPrivateKey(config *ssh.ServerConfig) {
 
-	f, err := os.OpenFile(logfile, os.O_APPEND | os.O_WRONLY, 0644)
+	homeDirectory := os.Getenv("HOME")
+
+	privateBytes, err := ioutil.ReadFile(homeDirectory + "/.ssh/id_rsa")
 	if err != nil {
-		log.Println(err)
-		return
+
+		log.Fatal("Fail to load private key: ", err)
+
 	}
 
-	defer f.Close()
-
-	_, err = f.WriteString(buf.String())
+	fmt.Print("Enter passphrase: ")
+	passBytes, err := terminal.ReadPassword(0)
 	if err != nil {
-		log.Println(err)
-		return
-	}
-}
 
-func handleChannel(conn *ssh.ServerConn, newChannel ssh.NewChannel) {
-	channel, reqs, err := newChannel.Accept()
+		log.Fatal("Fail to read password: ", err)
+
+	}
+	fmt.Println()
+
+	privateKeySSL, err := openssl.LoadPrivateKeyFromPEMWidthPassword(privateBytes, string(passBytes))
 	if err != nil {
-		log.Println("Could not accept channel: ", err)
-		return
+
+		log.Fatal("Fail to parse private key: ", err)
+
 	}
-	defer channel.Close()
-	for req := range reqs {
-		switch req.Type {
-		case "exec":
-			if req.WantReply {
-				req.Reply(true, nil)
-			}
-			var buf bytes.Buffer
-			logger := log.New(&buf, "", log.LstdFlags)
-			logger.Println(string(req.Payload[4:]))
-			writeLog(buf)
 
-			args := strings.Split(string(req.Payload[4:]), " ")
-			if args[0] == "init" {
-				return
-			}
-			if args[0] != "git-receive-pack" && args[0] != "git-upload-pack" {
-				channel.Stderr().Write([]byte("Sorry, this server just accept git requests.\n"))
-				return
-			}
-			args[1] = strings.Replace(args[1], "'", "", -1)
-
-			cmd := exec.Command("git-shell", "-c", args[0]+" '"+os.Getenv("HOME")+"/git/"+args[1]+"'")
-			pipeCmd(cmd, channel, channel.Stderr(), channel)
-			cmd.Start()
-			status, err := exitStatus(cmd.Wait())
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			if _, err := channel.SendRequest("exit-status", false, ssh.Marshal(&status)); err != nil {
-				log.Println("sendExit, ", err)
-				return
-			}
-			return
-		case "env":
-			if req.WantReply {
-				req.Reply(true, nil)
-			}
-		case "shell":
-			channel.Stderr().Write([]byte("Sorry, this server just accept exec requests.\n"))
-			return
-		}
-	}
-}
-
-type exitStatusMsg struct {
-	Status uint32
-}
-
-func exitStatus(err error) (exitStatusMsg, error) {
+	privateBytes, err = privateKeySSL.MarshalPKCS1PrivateKeyPEM()
 	if err != nil {
-		if exitErr, ok := err.(*exec.ExitError); ok {
-			if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
-				return exitStatusMsg{uint32(status.ExitStatus())}, nil
-			}
-		}
-		return exitStatusMsg{0}, err
+
+		log.Fatal("Fail to load private key: ", err)
+
 	}
-	return exitStatusMsg{0}, nil
+
+	privateKey, err := ssh.ParsePrivateKey(privateBytes)
+	if err != nil {
+
+		log.Fatal("Fail to parse private key: ", err)
+
+	}
+
+	config.AddHostKey(privateKey)
+
+	log.Println("Private key loaded.")
+
 }
 
-func pipeCmd(cmd *exec.Cmd, stdout, stderr io.Writer, stdin io.Reader) {
+func restClient(method string, url *url.URL, jsonStr []byte) (*http.Response, error) {
+
+	request, err := http.NewRequest(method, url.String(), bytes.NewBuffer(jsonStr))
+	if err != nil {
+
+		return nil, fmt.Errorf("Fail to create new request: ", err)
+
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+
+		return nil, fmt.Errorf("Fail to do request: ", err)
+
+	}
+
+	return response, nil
+
+}
+
+func publicKeyCallback(connectionMetadata ssh.ConnMetadata, key ssh.PublicKey) (*ssh.Permissions, error) {
+
+	publicKey = base64.StdEncoding.EncodeToString(key.Marshal())
+	url, err := url.Parse("http://" + authServer + ":3000/api/authentication")
+	if err != nil {
+
+		return nil, fmt.Errorf("Fail to parse url: ", err)
+
+	}
+
+	jsonStr := []byte(fmt.Sprintf(`{"key":{"key":"%s"}}`, publicKey))
+	response, err := restClient("POST", url, jsonStr)
+	if err != nil {
+
+		return nil, err
+
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode == 200 {
+
+		log.Println(connectionMetadata.User() + " accepted.")
+		return nil, nil
+
+	}
+
+	return nil, fmt.Errorf("Public key rejected for %q.", connectionMetadata.User())
+
+}
+
+func main() {
+
+	config := &ssh.ServerConfig{
+
+		PublicKeyCallback: publicKeyCallback,
+
+	}
+
+	authServer = os.Getenv("AUTH_SERVER")
+	if authServer == "" {
+
+		authServer = "127.0.0.1"
+
+	}
+
+	loadPrivateKey(config)
+	createServer(config)
+
+}
+
+func pipeCommand(cmd *exec.Cmd, stdout, stderr io.Writer, stdin io.Reader) {
+
 	stdinIn, err := cmd.StdinPipe()
 	if err != nil {
 		log.Println(err)
@@ -190,18 +373,5 @@ func pipeCmd(cmd *exec.Cmd, stdout, stderr io.Writer, stdin io.Reader) {
 	}()
 	go io.Copy(stdout, stdoutOut)
 	go io.Copy(stderr, stderrOut)
-}
 
-func main() {
-	config := &ssh.ServerConfig{
-		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
-			if c.User() == "git" && string(pass) == os.Getenv("GIT_PASSWD") {
-				return nil, nil
-			}
-			return nil, fmt.Errorf("password rejected for %q", c.User())
-		},
-	}
-
-	parseKeys(config)
-	createServer(config)
 }
